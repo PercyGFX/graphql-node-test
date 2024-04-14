f1b665196cf46cfe1cc6dc950e85f1ddec8c6bbb
PercyGFX <isurangabtk@gmail.com>
2024-04-14 15:54:54 +0530

    graphql apollo setup with express

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import { typeDefs, resolvers } from "./graphql.js";
+import { ApolloServer } from "apollo-server-express";
+
+const app = express()
+
+const server = new ApolloServer({ typeDefs, resolvers });
+await server.start();
+server.applyMiddleware({ app });
+
+
+app.get('/',(req,res)=>{
+
+    res.send("helloo")
+
+})
+
+
+const port = process.env.PORT || 5000; // Default to port 3000 if PORT environment variable is not set
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
