23026af6285d90dff5d45d2f4745f35478fd4962
PercyGFX <isurangabtk@gmail.com>
2025-01-02 16:31:42 +0530

    students

--- src/graphql.ts
+++ src/graphql.ts
@@ -1,14 +1,30 @@
 import { gql } from "apollo-server-express";
 
 const typeDefs = gql`
+  type Student {
+    id: Int!
+    name: String!
+    age: Int!
+  }
+
   type Query {
-    hello: String
+    students: [Student!]!
+    student(id: Int!): Student
   }
 `;
 
 const resolvers = {
   Query: {
-    hello: () => "Hello, world!",
+    students: () => {
+      // Return array of students
+      return [
+        { id: 1, name: "John", age: 20 },
+        { id: 2, name: "Jane", age: 21 },
+      ];
+    },
+    student: (_, { id }) => {
+      return { id: id, name: "John", age: 20 };
+    },
   },
 };
 
